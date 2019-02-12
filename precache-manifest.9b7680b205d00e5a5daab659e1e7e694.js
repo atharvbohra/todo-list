a08eb2157bff90afade014a7dbf8a52dbafe2996
atharvbohra <deepakbohra82@gmail.com>
2019-02-12 18:30:31 +0530

    Updates

---
+++ precache-manifest.9b7680b205d00e5a5daab659e1e7e694.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "86494e9c6d32fb67be2b",
+    "url": "/todo-list/static/js/runtime~main.86494e9c.js"
+  },
+  {
+    "revision": "25ad5fb72f0e7aea1b39",
+    "url": "/todo-list/static/js/main.25ad5fb7.chunk.js"
+  },
+  {
+    "revision": "1c7f25041e1c483d64f1",
+    "url": "/todo-list/static/js/2.1c7f2504.chunk.js"
+  },
+  {
+    "revision": "25ad5fb72f0e7aea1b39",
+    "url": "/todo-list/static/css/main.01dc84fa.chunk.css"
+  },
+  {
+    "revision": "c411c33bbdd749e915f19d40e25f35f9",
+    "url": "/todo-list/index.html"
+  }
+];
